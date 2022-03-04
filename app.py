4613d301aeb46195bc5a657b77ae3bb7b6a55fd1
Gaurav Kabra <gauravkabra.official@gmail.com>
2022-03-04 17:31:12 +0530

    1

---
+++ app.py
@@ -0,0 +1,10 @@
+import flask
+from flask import request
+
+
+app = flask.Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def home():
+    img_bytes = request.args['arg1']
+    return 'Hello World ' + type(img_bytes) + " " + str(img_bytes)
